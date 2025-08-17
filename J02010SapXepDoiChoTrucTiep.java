5435b3befe926db32daeff3fc3b1a48b8cba6fe7
Pe <nguyenbatho2k5@gmail.com>
2025-08-17 20:37:06 +0700

    J02010

---
+++ J02010SapXepDoiChoTrucTiep.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class J02010SapXepDoiChoTrucTiep {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+    
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0 ;i < n;i++) a[i] = sc.nextInt();
+        for (int i = 0 ;i < n;i++) {
+            int swapped = 0;
+            for (int j = i + 1 ;j < n;j++) {
+                if (a[i] > a[j]) {
+                    int tmp = a[i];
+                    a[i] = a[j];
+                    a[j] = tmp;
+                }
+                swapped = 1;
+            }
+
+            if (swapped == 1) {
+                System.out.print("Buoc " + (i+1) + ": ");
+                for (int k = 0 ; k < n;k++) {
+                    System.out.print(a[k] + " ");
+                }
+                System.out.println("");
+            }
+        }
+    }
+
+}
