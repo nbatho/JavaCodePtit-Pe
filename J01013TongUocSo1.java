c2689cbc23e51892abad09ac83a7891c9cbe66cc
Pe <nguyenbatho2k5@gmail.com>
2025-08-19 07:59:43 +0700

    J03008

---
+++ J01013TongUocSo1.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class J01013TongUocSo1 {
+    static final int MAX = 2000000; // tùy theo giới hạn đề
+    static int[] spf = new int[MAX + 1];
+
+    // Tiền xử lý SPF
+    static void sieve() {
+        for (int i = 2; i <= MAX; i++) spf[i] = i;
+        for (int i = 2; i * i <= MAX; i++) {
+            if (spf[i] == i) { 
+                for (int j = i * i; j <= MAX; j += i) {
+                    if (spf[j] == j) spf[j] = i;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sieve();
+
+        int tc = sc.nextInt();
+        long ans = 0; // nên để long nếu tổng lớn
+        for (int t = 0; t < tc; t++) {
+            int n = sc.nextInt();
+            while (n > 1) {
+                ans += spf[n];
+                n /= spf[n];
+            }
+        }
+        System.out.println(ans);
+    }
+}
