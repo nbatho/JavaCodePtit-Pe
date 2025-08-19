db319a574c31055c12e3084dc3076628e8e5f064
Pe <nguyenbatho2k5@gmail.com>
2025-08-19 09:01:57 +0700

    J02017

---
+++ J02017ThuGonDaySo.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class J02017ThuGonDaySo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+
+        Stack<Integer> st = new Stack<>();
+        for (int x : a) {
+            if (!st.isEmpty() && (st.peek() + x) % 2 == 0) {
+                st.pop(); 
+            } else {
+                st.push(x); 
+            }
+        }
+
+        System.out.println(st.size());
+    }
+}
