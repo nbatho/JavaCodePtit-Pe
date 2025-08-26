aa9ec2479b640288ecb4ef028d7f347968c466ec
Pe <nguyenbatho2k5@gmail.com>
2025-08-26 10:22:28 +0700

    J05003

---
+++ J05003DanhSachDoiTuongSinhVien1.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class J05003DanhSachDoiTuongSinhVien1 {
+        static class SV {
+        String msv;
+        String ten,lop,ns;
+        double gpa;
+        String dd,yyyy,mm;
+        SV(int stt,String msv,String ten, String lop,String ns,double gpa) {
+            this.msv = msv + String.format("%03d", stt);
+            this.ten = ten;
+            this.lop = lop;
+            this.ns = ns;
+            String s[] = this.ns.split("/");
+            this.dd = s[0];
+            if (this.dd.length() < 2) {
+                this.dd = "0" + this.dd;
+            }
+            this.mm = s[1];
+            if (this.mm.length() < 2) {
+                this.mm = "0" + this.mm;
+            }
+            this.yyyy = s[2];
+            this.gpa = gpa;
+        }
+        void op() {
+            System.out.printf("%s %s %s %s/%s/%s %.2f\n",msv,ten,lop,dd,mm,yyyy,gpa);
+        }
+    }
+    public static void main(String[] args) {
+        try {
+            
+            Scanner sc = new Scanner(System.in);
+    
+            int tc = sc.nextInt();
+            for (int i = 0 ;i < tc;i++) {
+                sc.nextLine();
+                String ten = sc.nextLine();
+                String lop = sc.nextLine();
+                String ns = sc.nextLine();
+                double gpa = sc.nextDouble();
+                SV a = new SV(i+1, "B20DCCN", ten, lop, ns, gpa);
+
+                a.op();
+
+            }
+        } catch (Exception e) {
+        }
+    }
+}
