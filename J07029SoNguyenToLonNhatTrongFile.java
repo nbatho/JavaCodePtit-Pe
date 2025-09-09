d643b41b09c17189f22981b19dc66b6cee94352b
Pe <nguyenbatho2k5@gmail.com>
2025-09-09 10:28:30 +0700

    J07029

---
+++ J07029SoNguyenToLonNhatTrongFile.java
@@ -0,0 +1,31 @@
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
+import java.util.TreeMap;
+
+public class J07029SoNguyenToLonNhatTrongFile {
+    static boolean isPrime(int n) {
+        if (n < 2) return false;
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+    public static void main(String[] args) throws Exception{
+        ObjectInputStream ois = new ObjectInputStream(new FileInputStream("SONGUYEN.in"));
+
+        ArrayList <Integer> list = (ArrayList<Integer>) ois.readObject();
+
+        TreeMap<Integer, Integer> mp = new TreeMap<>();
+        for (int x : list) {
+            if (isPrime(x))
+                mp.put(x, mp.getOrDefault(x, 0) + 1);
+        }
+
+        for (int i = 0; i < 10 && !mp.isEmpty(); i++) {
+            int k = mp.lastKey();
+            System.out.println(k + " " + mp.get(k));
+            mp.remove(k);
+        }
+    }
+}
