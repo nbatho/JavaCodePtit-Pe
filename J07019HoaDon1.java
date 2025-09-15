640a8de040e04e6f2b3be44db2d903133456b1b9
Pe <nguyenbatho2k5@gmail.com>
2025-09-15 09:33:19 +0700

    J07019

---
+++ J07019HoaDon1.java
@@ -0,0 +1,81 @@
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class J07019HoaDon1 {
+    static class SP {
+        String ma, tenSP;
+        long gl1, gl2;
+
+        SP(String ma, String tenSP, long gl1, long gl2) {
+            this.ma = ma;
+            this.tenSP = tenSP;
+            this.gl1 = gl1;
+            this.gl2 = gl2;
+        }
+    }
+
+    static class HD {
+        String maHD, tenSP;
+        long gg, tra;
+
+        HD(String maHD, SP sp, long slm, int loai, int stt) {
+            this.maHD = maHD + "-" + String.format("%03d", stt);
+            long tt = (loai == 1 ? slm * sp.gl1 : slm * sp.gl2);
+            long giam = 0;
+            if (slm >= 150) giam = tt * 50 / 100;
+            else if (slm >= 100) giam = tt * 30 / 100;
+            else if (slm >= 50) giam = tt * 15 / 100;
+            this.gg = giam;
+            this.tra = tt - giam;
+            this.tenSP = sp.tenSP;
+        }
+
+        void op() {
+            System.out.printf("%s %s %d %d\n", maHD, tenSP, gg, tra);
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            
+        
+        File f1 = new File("DATA1.in");
+        Scanner sc1 = new Scanner(f1);
+
+        int tc = sc1.nextInt();
+        sc1.nextLine();
+        Map<String, SP> map = new HashMap<>();
+        for (int i = 0; i < tc; i++) {
+            String ma = sc1.nextLine();
+            String ten = sc1.nextLine();
+            long g1 = sc1.nextLong(), g2 = sc1.nextLong();
+            sc1.nextLine();
+            map.put(ma, new SP(ma, ten, g1, g2));
+        }
+
+
+        File f2 = new File("DATA2.in");
+        Scanner sc = new Scanner(f2);
+        int slhd = sc.nextInt();
+        sc.nextLine();
+        int stt = 1;
+        for (int i = 0; i < slhd; i++) {
+            String[] tmp = sc.nextLine().split(" ");
+            String fullMa = tmp[0];
+            String maSP = fullMa.substring(0, 2);
+            int loai = fullMa.charAt(2) - '0';
+            long slm = Long.parseLong(tmp[1]);
+
+            SP sp = map.get(maSP);
+            if (sp != null) {
+                HD hd = new HD(fullMa, sp, slm, loai, stt++);
+                hd.op();
+            }
+        }
+
+        } catch (Exception e) {
+        }
+    }
+}
